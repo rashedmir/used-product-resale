a1ce9f3b4f4395dce3c43fef23c8cceb2ee42f99
rashedmir <rashedmir89@gmail.com>
2022-11-26 19:38:14 +0600

    Routes error corrected

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -2,15 +2,11 @@ import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import Homepage from '../../Homepage/Homepage';
 import Main from '../../Main/Main';
-// import LuxuryWatchesMap from '../../Pages/LuxuryWatches/LuxuryWatchesMap';
 import VintageRolex from '../../Pages/VintageRolex/VintageRolex';
 import Login from '../../Pages/Login/Login'
 import Register from '../../Pages/Register/Register';
 import AddProducts from '../../Pages/AddProducts/AddProducts'
-import MyProductsMap from '../../Pages/MyProducts/MyProductsMap';
 import PrivateRoute from '../PrivateRoute/PrivateRoute';
-// import RolexMap from '../../Pages/Rolex/RolexMap';
-// import OmegaMap from '../../Pages/Omega/OmegaMap';
 import Omega from '../../Pages/Omega/Omega';
 import Rolex from '../../Pages/Rolex/Rolex';
 import LuxuryWatches from '../../Pages/LuxuryWatches/LuxuryWatches'
@@ -50,7 +46,7 @@ export const routes = createBrowserRouter([
                 element: <PrivateRoute><AddProducts></AddProducts></PrivateRoute>
             },
             {
-                path:'/homepage/myproduct',
+                path: '/homepage/myproduct',
                 element: <PrivateRoute><MyProducts></MyProducts></PrivateRoute>,
                 loader: () => fetch(`http://localhost:5000/watches`)
             }
